ba3538073477343db17f44809d7dac1ca7b4c445
Neezar <rforlyffe@gmaicl.com>
2020-07-12 15:59:41 +0800

    complete home section

--- src/App/features/data/index.ts
+++ src/App/features/data/index.ts
@@ -1,4 +1,10 @@
 import ReactLogo from "./../../../images/sprite.svg";
+import H1 from "./../../../images/house-1.jpeg";
+import H2 from "./../../../images/house-2.jpeg";
+import H3 from "./../../../images/house-3.jpeg";
+import H4 from "./../../../images/house-4.jpeg";
+import H5 from "./../../../images/house-5.jpeg";
+import H6 from "./../../../images/house-6.jpeg";
 
 export const featureData = [
   {
@@ -48,5 +54,68 @@ export const featureData = [
       "Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia quae.",
     svg: ReactLogo,
     svgClass: "#icon-lock",
+  },
+];
+
+export const HomeData = [
+  {
+    id: 1,
+    title: "Beautiful Familiy House",
+    p1: "USA",
+    p2: "5 rooms",
+    p3: "325 m",
+    p4: "$1,200,000",
+    contact: "Contact realtor",
+    image: `${H1}`,
+  },
+  {
+    id: 2,
+    title: "Modern Glass Villa",
+    p1: "Canada",
+    p2: "6 rooms",
+    p3: "450 m",
+    p4: "$2,200,000",
+    contact: "Contact realtor",
+    image: `${H2}`,
+  },
+  {
+    id: 3,
+    title: "Cozy Country House",
+    p1: "UK",
+    p2: "2 rooms",
+    p3: "100 m",
+    p4: "$1,400,000",
+    contact: "Contact realtor",
+    image: `${H3}`,
+  },
+  {
+    id: 4,
+    title: "Large Rustical Villa",
+    p1: "Portugal",
+    p2: "4 rooms",
+    p3: "800 m",
+    p4: "$400,000",
+    contact: "Contact realtor",
+    image: `${H4}`,
+  },
+  {
+    id: 5,
+    title: "Majestic Palace House",
+    p1: "Germany",
+    p2: "no rooms",
+    p3: "100 m",
+    p4: "$1,200,000",
+    contact: "Contact realtor",
+    image: `${H5}`,
+  },
+  {
+    id: 6,
+    title: "Modern Familiy Apartment",
+    p1: "Italy",
+    p2: "6 rooms",
+    p3: "340 m",
+    p4: "$1,200,000",
+    contact: "Contact realtor",
+    image: `${H6}`,
   },
 ];
