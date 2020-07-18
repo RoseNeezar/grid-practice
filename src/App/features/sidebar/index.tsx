2f5186e232e98ada30fa697875d4a1825c5c933a
Neezar <rforlyffe@gmaicl.com>
2020-07-18 22:46:54 +0800

    complete footer and add sidebar button

--- src/App/features/sidebar/index.tsx
+++ src/App/features/sidebar/index.tsx
@@ -5,10 +5,39 @@ const SidebarLayout = styled.div`
   background-color: ${({ theme }) => theme.colors.primary};
   grid-column: sidebar-start / sidebar-end;
   grid-row: 1/ -1;
+  display: flex;
+  justify-content: center;
+`;
+const SidebarButton = styled.button`
+  border-radius: 0;
+  border: none;
+  background-color: #fff;
+  height: 2px;
+  width: 4.5rem;
+  margin-top: 4rem;
+
+  &::before,
+  &::after {
+    background-color: #fff;
+    height: 2px;
+    width: 4.5rem;
+    content: "";
+    display: block;
+  }
+  &::before {
+    transform: translateY(-1.5rem);
+  }
+  &::after {
+    transform: translateY(1.3rem);
+  }
 `;
 
 const Sidebar = () => {
-  return <SidebarLayout>Sidebar</SidebarLayout>;
+  return (
+    <SidebarLayout>
+      <SidebarButton />
+    </SidebarLayout>
+  );
 };
 
 export default Sidebar;
